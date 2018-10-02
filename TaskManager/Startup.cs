790059a55acaa006852b89dee4923a7578b41695
Samuel Silva De Carvalho <samuel.carvalho@consinco.com.br>
2018-10-02 14:37:31 -0300

    adição do swagger

--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using TaskManager.Context;
 
 namespace TaskManager
@@ -46,6 +48,19 @@ namespace TaskManager
                 opt.UseNpgsql(Configuration.GetConnectionString("conPostgree" ), 
                 o=> o.MaxBatchSize(int.MaxValue))
             );
+
+            // Configure Swagger
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new Info {
+                    Title = "TaskManager API",
+                        Version = "v1",
+                        Description = " "                    
+                });           
+
+                var xmlFile = Path.ChangeExtension (typeof (Startup).Assembly.Location, ".xml");
+                c.IncludeXmlComments (xmlFile);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +74,12 @@ namespace TaskManager
             {
                 app.UseHsts();
             }
+            app.UseMvc ();
+            
+            app.UseSwagger ();
+            app.UseSwaggerUI (c => {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Swagger Sample");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
